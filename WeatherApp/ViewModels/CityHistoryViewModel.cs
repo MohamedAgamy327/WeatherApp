0f70d40e6ffd9bb14fdf51de937522450ee81b4e
Mohamed Agamy <mohamed.agamy@vcontractor.co.za>
2022-05-21 20:10:13 +0200

    history

--- WeatherApp/ViewModels/CityHistoryViewModel.cs
+++ WeatherApp/ViewModels/CityHistoryViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.IServices;
+using WeatherApp.Models;
 using Windows.UI.Xaml.Navigation;
 
 namespace WeatherApp.ViewModels
@@ -12,12 +16,50 @@ namespace WeatherApp.ViewModels
     public class CityHistoryViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
+        private readonly IWeatherService _weatherService;
 
-        public CityHistoryViewModel(INavigationService navigationService)
+        public CityHistoryViewModel(
+            INavigationService navigationService,
+            IWeatherService weatherService)
         {
             _navigationService = navigationService;
+            _weatherService = weatherService;
+
+            _selectedCity = App.City;
+            var task = _weatherService.GetAll(App.City);
+            task.Wait();
+            List<Weather> weathers = task.Result;
+            _Weathers = new ObservableCollection<Weather>(weathers);
         }
 
+        private string _selectedCity;
+        public string SelectedCity
+        {
+            get { return _selectedCity; }
+            set { Set(ref _selectedCity, value); }
+        }
+
+        private ObservableCollection<Weather> _Weathers;
+        public ObservableCollection<Weather> Weathers
+        {
+            get { return _Weathers; }
+            set { Set(ref _Weathers, value); }
+        }
+
+
+        private RelayCommand _navigateToMainView;
+        public RelayCommand NavigateToMainView
+        {
+            get
+            {
+                return _navigateToMainView
+                    ?? (_navigateToMainView = new RelayCommand(NavigateToMainViewMethod));
+            }
+        }
+        private void NavigateToMainViewMethod()
+        {
+            _navigationService.GoBack();
+        }
 
     }
 }
