1f5a38894512e09fe023a063f6cb8772e3a85f5e
Mohamed Agamy <mohamed.agamy@vcontractor.co.za>
2022-05-21 20:56:57 +0200

    details

--- WeatherApp/ViewModels/CityDetailsViewModel.cs
+++ WeatherApp/ViewModels/CityDetailsViewModel.cs
@@ -1,23 +1,48 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.UI.Xaml.Navigation;
+using WeatherApp.IServices;
+using WeatherApp.Models;
 
 namespace WeatherApp.ViewModels
 {
     public class CityDetailsViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
+        private readonly IWeatherService _weatherService;
 
-        public CityDetailsViewModel(INavigationService navigationService)
+        public CityDetailsViewModel(
+            INavigationService navigationService,
+            IWeatherService weatherService)
         {
             _navigationService = navigationService;
+            _weatherService = weatherService;
+
+            var task = _weatherService.GetCurrent(App.City);
+            task.Wait();
+            _currentWeather = task.Result;
         }
 
+        private Weather _currentWeather;
+        public Weather CurrentWeather
+        {
+            get { return _currentWeather; }
+            set { Set(ref _currentWeather, value); }
+        }
+
+        private RelayCommand _navigateToMainView;
+        public RelayCommand NavigateToMainView
+        {
+            get
+            {
+                return _navigateToMainView
+                    ?? (_navigateToMainView = new RelayCommand(NavigateToMainViewMethod));
+            }
+        }
+        private void NavigateToMainViewMethod()
+        {
+            _navigationService.GoBack();
+        }
 
     }
 }
